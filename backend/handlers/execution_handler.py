d242314b79726a72238948e98857c1e55ce01b38
FrirexD <frirex.go@gmail.com>
2024-10-26 17:10:56 +0200

    C compiler and graphic changes

--- backend/handlers/execution_handler.py
+++ backend/handlers/execution_handler.py
@@ -4,12 +4,21 @@ def register_execution_handlers(socketio):
     @socketio.on('execute_code')
     def handle_execute_code(data):
         try:
-            result = ExecutionService.execute_python_file(
-                data.get('project'),
-                data.get('filename'),
-                data.get('code')
-            )
-            socketio.emit('code_output', result)
+            if data.get('filename').endswith(".py") : 
+                result = ExecutionService.execute_python_file(
+                    data.get('project'),
+                    data.get('filename'),
+                    data.get('code')
+                )
+                socketio.emit('code_output', result)
+            if data.get('filename').endswith(".c") : 
+                result = ExecutionService.execute_c_file(
+                    data.get('project'),
+                    data.get('filename'),
+                    data.get('code')
+                )
+                socketio.emit('code_output', result)
+        
         except Exception as e:
             socketio.emit('code_output', {
                 'error': str(e),
